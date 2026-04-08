90e0a35c363843b99a3dcd4ab7209440c6ea3120
vblaws <h3239498031@gmail.com>
2026-04-08 18:34:13 +0800

    'init'

---
+++ src/main/java/cloud/hexiaolei/ecommercesystem_backend/ECommerceSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package cloud.hexiaolei.ecommercesystem_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSystemBackendApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceSystemBackendApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/cloud/hexiaolei/ecommercesystem_backend/ECommerceSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.hexiaolei.ecommercesystem_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSystemBackendApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
